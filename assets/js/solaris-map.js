e365269a1ed6037814cbba3b0ee90cceafd5e49a
benhur07b <benhur07b@users.noreply.github.com>
2017-12-03 03:07:50 +0800

    Added new zoom functions (full extent + bounding box). Added draw function. Added function to extract coordinates of vertices of the drawn polygon. Added geocoding.

--- assets/js/solaris-map.js
+++ assets/js/solaris-map.js
@@ -15,6 +15,8 @@ var solar_ann = L.tileLayer('http://api.solar.padayon.ph/tiles/{z}/{x}/{y}.png',
     tms: true
 });
 
+var drawn_feature = L.featureGroup()
+
 var basemaps = {
     "OSM": osm,
     "Google": google
@@ -22,6 +24,7 @@ var basemaps = {
 
 var overlaymaps = {
     "Solar Energy (GHI)": solar_ann,
+    "Drawn Feature": drawn_feature
 }
 
 var map = L.map('map', {
@@ -29,14 +32,87 @@ var map = L.map('map', {
     zoom: 11,
     minZoom: 8,
     maxZoom: 18,
-    zoomControl: false,
+    zoomControl: true,
     maxBounds: [[14.3, 120.9], [14.9, 121.2]],
     maxBoundsViscosity: 0.5,
-    layers: [google, solar_ann]
+    layers: [google, solar_ann, drawn_feature]
 });
 
-L.control.layers(basemaps, overlaymaps).addTo(map);
-L.control.scale({position:'bottomleft', maxWidth:100}).addTo(map);
+L.control.layers(basemaps, overlaymaps, {collapsed: false}).addTo(map);
+L.control.scale({position:'bottomright', maxWidth:100}).addTo(map);
+
+// SIDEBAR
+// var sidebar = L.control.sidebar('sidebar').addTo(map);
+
+// EASYBUTTON ZOOM TO EXTENT
+L.easyButton({
+    states: [{
+        icon: 'fa-globe',
+        onClick: function(){
+            map.setView([14.56, 120.05], 11);},
+        title: 'Zoom to Extent'
+    }]
+}).addTo(map);
+
+// ZOOM BAR
+var options = {
+    modal: true,
+    title: "Zoom to Bounding Box"
+};
+var zoom_box = L.control.zoomBox(options);
+map.addControl(zoom_box);
+
+// DRAW
+var draw_control = new L.Control.Draw({
+    edit: {
+        featureGroup: drawn_feature,
+        allowIntersection: false,
+    },
+    draw: {
+        polygon: {
+            allowIntersection: false,
+            showArea: true
+        },
+        polyline: false,
+        rectangle: false,
+        circle: false,
+        marker: false,
+        circlemarker: false,
+    }
+});
+
+map.addControl(draw_control);
+
+// map.on(L.Draw.Event.CREATED, function (event) {
+//     var layer = event.layer;
+//
+//     drawn_feature.addLayer(layer);
+// });
+
+map.on(L.Draw.Event.CREATED, function (event) {
+    var layer = event.layer;
+
+    console.log(JSON.stringify(layer.toGeoJSON()));
+
+    drawn_feature.addLayer(layer);
+});
+
+// EASYBUTTON CALCULATE
+L.easyButton({
+    states: [{
+        icon: 'fa-sun-o',
+        onClick: function (){
+            // map.setView([14.56, 120.05], 11)
+            var drawn_features = drawn_feature.getLayers();
+            if(drawn_features.length == 1) {
+                var coords = drawn_features[0].toGeoJSON()['geometry']['coordinates'][0];
+                // console.log(JSON.stringify(drawn_features[0].toGeoJSON()));
+                console.log(JSON.stringify(coords));
+            }
+            ;},
+        title: 'GoSOLAR'
+    }]
+}).addTo(map);
 
 // GEOCODE SEARCH
 var arcgisOnline = L.esri.Geocoding.arcgisOnlineProvider({
@@ -61,8 +137,6 @@ var results = L.layerGroup().addTo(map);
 searchControl.on('results', function(data){
     results.clearLayers();
 });
-
-var zoom_bar = new L.Control.ZoomBar({position: 'topleft'}).addTo(map);
 
 function initToggle(){
     window.toggle = false;
