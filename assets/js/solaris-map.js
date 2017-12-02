cc4da01a1fe3f785fcea475872d49ddef8688c64
benhur07b <benhur07b@users.noreply.github.com>
2017-12-03 00:29:41 +0800

    Added new zoom and geocoding.

--- assets/js/solaris-map.js
+++ assets/js/solaris-map.js
@@ -1,7 +1,7 @@
 window.onload = initToggle();
 
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
+	maxZoom: 18,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
@@ -27,9 +27,9 @@ var overlaymaps = {
 var map = L.map('map', {
     center: [14.56, 120.05],
     zoom: 11,
-    minZoom: 5,
-    maxZoom: 19,
-    zoomControl: true,
+    minZoom: 8,
+    maxZoom: 18,
+    zoomControl: false,
     maxBounds: [[14.3, 120.9], [14.9, 121.2]],
     maxBoundsViscosity: 0.5,
     layers: [google, solar_ann]
@@ -37,6 +37,32 @@ var map = L.map('map', {
 
 L.control.layers(basemaps, overlaymaps).addTo(map);
 L.control.scale({position:'bottomleft', maxWidth:100}).addTo(map);
+
+// GEOCODE SEARCH
+var arcgisOnline = L.esri.Geocoding.arcgisOnlineProvider({
+    countries: 'PHL'
+});
+
+var searchControl = L.esri.Geocoding.geosearch({
+    providers: [
+        arcgisOnline,
+        L.esri.Geocoding.featureLayerProvider({
+            url: 'https://services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/gisday/FeatureServer/0/',
+            bufferRadius: 5000
+        })
+    ],
+    expanded: false,
+    position: 'topright',
+    collapseAfterResult: true
+}).addTo(map);
+
+var results = L.layerGroup().addTo(map);
+
+searchControl.on('results', function(data){
+    results.clearLayers();
+});
+
+var zoom_bar = new L.Control.ZoomBar({position: 'topleft'}).addTo(map);
 
 function initToggle(){
     window.toggle = false;
